bca49b602f6ba9e87e61d1583febe51b4be74850
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-02 11:10:21 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors');
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('<h1>The server is working!</h1>');
+});
+
+app.listen(PORT, ()=> {
+    console.log('server running @ localhost:' + PORT);
+}).on('error', (err)=>{
+    console.log('Server listen error, do you already have a server running on PORT' + PORT);
+});
